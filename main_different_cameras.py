b6b8168c8485eea6baef88410aae0ed94b4cd174
Maxim Lyuzin <mr.maxxe@gmail.com>
2022-08-25 18:54:36 +0300

    Add files via upload

--- main_different_cameras.py
+++ main_different_cameras.py
@@ -3,7 +3,7 @@ from camera_tools import *
 import cv2
 
 TEMPLATE_SIZE = (100, 100)
-COEF = 2.66 #f'_right/f'_left
+COEF = 2.66  # f'_right/f'_left
 
 D = dict.fromkeys(['x', 'y'])
 D['x'] = 500
@@ -62,37 +62,31 @@ if __name__ == "__main__":
 
                 click_x, click_y = D['x'], D['y']
 
-
                 ROI = left_image[click_y - TEMPLATE_SIZE[1]//2:click_y + TEMPLATE_SIZE[1]//2,
                                  click_x - TEMPLATE_SIZE[0]//2:click_x + TEMPLATE_SIZE[0]//2, :]
 
-                #r1, r2 = run_ccoeff_normed(right_image, ROI, verbose=False)
-                r1, r2 = run_ccoeff_normed(right_image, cv2.resize(ROI, (int(COEF*TEMPLATE_SIZE[0]), int(COEF*TEMPLATE_SIZE[1]))), verbose=False)
-                cv2.rectangle(right_image, r1, r2, 255, 2)
+                r1, r2 = run_ccoeff_normed(right_image,
+                                           cv2.resize(ROI, (int(COEF*TEMPLATE_SIZE[0]), int(COEF*TEMPLATE_SIZE[1]))),
+                                           verbose=False)
 
                 l1, l2 = (D['x'] - TEMPLATE_SIZE[0]//2, D['y'] - TEMPLATE_SIZE[0]//2),\
                          (D['x'] + TEMPLATE_SIZE[1]//2, D['y'] + TEMPLATE_SIZE[1]//2)
-                cv2.rectangle(left_image, l1, l2, (240, 240, 240), 2)
 
                 right_center = (r1[0] + COEF*TEMPLATE_SIZE[0]//2, r1[1] + COEF*TEMPLATE_SIZE[1]//2)
-                left_center = (D['x'], D['y'])
-                # disparity = abs(right_center[0] - left_center[0])
-
 
                 x_l_, y_l_ = click_x - 960/2, 540/2 - click_y
                 x_r_, y_r_ = right_center[0] - 960/2, 540/2 - right_center[1]
 
                 #print(right_center, x_r_, x_l_)
                 #disparity = abs(r1[0] - l1[0])
-                disparity = abs(x_r_/ COEF - x_l_)
-                # distance = 163-(-2.6359 + 15651.6209/disparity)
-                # distance = (-5.4576 + 14422.4746 / (1e-10 + disparity))
-                # distance = (-2.6359 + 15651.6209 / (1e-10 + disparity))
-                # distance = (-18.990 + 15723.5 / disparity)
+                disparity = abs(x_r_ / COEF - x_l_)
+
                 distance = 15682.42/(disparity+18.46)
                 print("#########################\n{} => {} sm".format(disparity, distance))
 
                 # Use numpy to place images next to each other
+                cv2.rectangle(right_image, r1, r2, 255, 2)
+                cv2.rectangle(left_image, l1, l2, (240, 240, 240), 2)
                 camera_images = np.hstack((left_image, right_image))
                 # Check to see if the user closed the window
                 # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
